e71ccf70b2a29f2baa3d66d2eafbfa2196b43687
Toonzaza <poppeth.peth@mail.kmutt.ac.th>
2024-10-04 17:48:44 +0700

    full

--- install/fun4_interfaces/share/fun4_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(fun4_interfaces_IDL_FILES "srv/Mode.idl")
-set(fun4_interfaces_INTERFACE_FILES "srv/Mode.srv;srv/Mode_Request.msg;srv/Mode_Response.msg")
