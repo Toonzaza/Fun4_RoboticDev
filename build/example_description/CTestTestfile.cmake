8742c73a8194a7628cb3ad484d7fdd1ab113356c
Toonzaza <poppeth.peth@mail.kmutt.ac.th>
2024-10-04 18:23:19 +0700

    full3.2

--- build/example_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/itoon/Documents/GitHub/Fun4_RoboticDev/src/example_description
-# Build directory: /home/itoon/Documents/GitHub/Fun4_RoboticDev/build/example_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/fun4_pkg/ament_cmake_python/fun4_pkg/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='fun4_pkg',
-    version='0.0.0',
-    packages=find_packages(
-        include=('fun4_pkg', 'fun4_pkg.*')),
-)
